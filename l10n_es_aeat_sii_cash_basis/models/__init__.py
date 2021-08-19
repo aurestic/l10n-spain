001de2295abb0d17e2f953ccaa9309e4e97d8562
almumu <almudena@aurestic.es>
2021-08-19 10:16:24 +0200

    nuevos módulos para criterio de caja

---
+++ l10n_es_aeat_sii_cash_basis/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account_invoice # noqa
+from . import account_payment_mode # noqa
+from . import account_payment_method # noqa
+from . import account_partial_reconcile # noqa
+from . import aeat_sii_payment_mode_key # noqa
